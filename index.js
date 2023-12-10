34ede9971132f941c0a8aa22fbfbdf0c12a47c92
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2023-12-10 19:43:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hola mundo")
+import { writeFile, readFile } from "node:fs/promises";
+import { argv } from "node:process";
+
+//const { operacion } = require('./operaciones.js')
+
+const [elementos] = argv.slice(2);
+//console.log(elementos);
+
+const addCita = async (elementos) => {
+    try {
+        const citas = JSON.parse(await readFile('citas.json'));
+        const cita = {
+            nombre: elementos, 
+            edad: elementos, 
+            animal: elementos, 
+            color: elementos, 
+            enfermedad: elementos
+        }
+        citas.push(cita);
+        await writeFile("citas.json", JSON.stringify(citas))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+addCita(elementos)
